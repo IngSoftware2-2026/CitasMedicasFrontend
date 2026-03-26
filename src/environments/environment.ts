95d3a3f38aee93cd21a1f45573b81a8224fe6bc1
. <juliocgp30@gmail.com>
2026-03-26 17:19:39 -0600

    Dejando el puerto del localhost de backend otra vez en 5096 No cambiar otra vez

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:9000',
+  apiUrl: 'http://localhost:5096',
   apiKey: '4b567cb1c6b24b51ab55248f8e66e5cc',
   cloudinary: {
     cloudName: 'djqcus2kl',
